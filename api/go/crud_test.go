f038c415c4adea23508bf37721372df6a5e27a7a
Kushuh <kuzanisu@gmail.com>
2020-12-26 19:40:10 +0100

    [FEATURE] Added SDK for Go (untested, partial)

--- api/go/crud_test.go
+++ api/go/crud_test.go
@@ -1,1 +1,48 @@
 package divanDocker
+
+import (
+	"fmt"
+	"github.com/docker/docker/api/types"
+	"path"
+	"runtime"
+	"testing"
+)
+
+func kill (id string) error {
+	if err := cli.ContainerKill(ctx, id, ""); err != nil {
+		return fmt.Errorf("unable to kill container : %s", err.Error())
+	}
+
+	if err := cli.ContainerRemove(ctx, id, types.ContainerRemoveOptions{}); err != nil {
+		return fmt.Errorf("unable to kill container : %s", err.Error())
+	}
+
+	return nil
+}
+
+func TestCreate(t *testing.T) {
+	_, filename, _, ok := runtime.Caller(0)
+	if !ok {
+		t.Fatalf("unable to get caller path")
+	}
+
+	config := path.Join(path.Dir(filename), "test_files")
+
+	id, err := Create("", nil)
+	if err == nil {
+		if err := kill(id); err != nil {
+			t.Fatalf(err.Error())
+		}
+
+		t.Fatalf("container is created despite empty config path")
+	}
+
+	id, err = Create(config, nil)
+	if err != nil {
+		t.Fatalf("unable to create container : %s", err.Error())
+	}
+
+	if err := kill(id); err != nil {
+		t.Fatalf(err.Error())
+	}
+}
--- api/go/main.go
+++ api/go/main.go
@@ -1,1 +1,34 @@
 package divanDocker
+
+import (
+	"context"
+	"github.com/a-novel/errors"
+	"github.com/docker/docker/client"
+)
+
+const (
+	Version = "1.0.0"
+)
+
+var cli *client.Client
+var ctx context.Context
+var err error
+var inited bool
+
+func Init() *errors.Error {
+	if inited {
+		return nil
+	}
+
+	// Use context to run Docker container.
+	ctx = context.Background()
+
+	// Launch docker client for Go.
+	cli, err = client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		return errors.New(ErrCannotRunDocker, err.Error())
+	}
+
+	inited = true
+	return nil
+}
