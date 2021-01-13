52f38deb8b2fcc90dd3ecf1164ce55426aa12e33
Kushuh <kuzanisu@gmail.com>
2021-01-13 19:39:32 +0100

    [CLEANUP] Moved api to another package : https://github.com/a-novel/divan-docker-sdk

--- api/go/errors.go
+++
@@ -1,7 +0,0 @@
-package divanDocker
-
-const (
-	ErrCannotRunDocker     = "err_cannot_run_docker"
-	ErrCannotPullImage     = "err_cannot_pull_image"
-	ErrCannotCreatePortMap = "err_cannot_create_port_map"
-)
